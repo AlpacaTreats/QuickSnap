cf4ccadf2c0f116443025f813ffae697dc6696d0
jlstant <jeanlucstanton@gmail.com>
2018-08-07 14:22:04 +1000

    fixed merge conflict error, couldn't start, but starts now

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -23,7 +23,7 @@ namespace CardGames
 			//Fetch the next batch of UI interaction
 			SwinGame.ProcessEvents ();
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE)) {
-				myGame.FlipNextCard ();
+				myGame.Start ();
 			}
 			if (myGame.IsStarted) {
 				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
